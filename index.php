82b9054767fd7078ce9f7b673ae426d1cfacb988
Kyle E. Mitchell <kyle@kemitchell.com>
2019-07-08 18:49:51 -0700

    index: Show post content

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php get_header(); ?>
-<?php while(have_posts()): ?>
-  <?php the_post(); ?>
+<?php while(have_posts()): the_post(); ?>
+  <?php the_content(); ?>
 <?php endwhile ?>
 <?php get_footer(); ?>
