1372c60497879bb17213ea8be7c280246196185e
Kyle E. Mitchell <kyle@kemitchell.com>
2019-07-08 19:11:26 -0700

    Conditionally display page titles

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php get_header(); ?>
 <?php if (have_posts()): while(have_posts()): the_post(); ?>
+  <?php if (!is_home() && !is_front_page()): ?>
+    <h1><?php the_title(); ?></h1>
+  <?php endif; ?>
   <?php the_content(); ?>
 <?php endwhile; endif; ?>
 <?php get_footer(); ?>
