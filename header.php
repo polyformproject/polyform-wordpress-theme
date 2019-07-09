9648b3059a20da2054a1785841c72b661b349be1
Kyle E. Mitchell <kyle@kemitchell.com>
2019-07-08 18:46:55 -0700

    Fill out <nav>

--- header.php
+++ header.php
@@ -11,8 +11,10 @@
       <img src="<?php echo get_template_directory_uri(); ?>/images/polyform.svg" alt="Polyform">
     </header>
     <nav role=navigation>
-      <ol>
+      <ul>
         <li><a href=/>Home</a></li>
-      </ol>
+        <li><a href=/licenses>Licenses</a></li>
+        <li><a href=https://github.com/polyform-project>GitHub</a></li>
+      </ul>
     </nav>
     <main role=main>
