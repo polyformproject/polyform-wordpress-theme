426969789c5a32f21ef4c28ac6e8af832aa15d13
Kyle E. Mitchell <kyle@kemitchell.com>
2019-07-08 18:59:51 -0700

    Refactor

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php get_header(); ?>
-<?php while(have_posts()): the_post(); ?>
+<?php if (have_posts()): while(have_posts()): the_post(); ?>
   <?php the_content(); ?>
-<?php endwhile ?>
+<?php endwhile; endif; ?>
 <?php get_footer(); ?>
