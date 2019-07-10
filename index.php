24babb792ae4b09ca73c5bbc4de2c191f910bdc5
Kyle E. Mitchell <kyle@kemitchell.com>
2019-07-09 18:27:12 -0700

    Show titles of non-front pages

--- index.php
+++ index.php
@@ -11,6 +11,9 @@
   </ol>
 <?php else: ?>
   <?php if (have_posts()): while(have_posts()): the_post(); ?>
+    <?php if(!is_front_page()): ?>
+      <h2><?php the_title(); ?></h2>
+    <?php endif; ?>
     <?php the_content(); ?>
   <?php endwhile; endif; ?>
 <?php endif; ?>
