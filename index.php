90036c4f479dae3171a8115168c76d71f8397830
Kyle E. Mitchell <kyle@kemitchell.com>
2019-07-09 18:26:10 -0700

    Cover all cases in index.php

--- index.php
+++ index.php
@@ -1,12 +1,17 @@
 <?php get_header(); ?>
-<?php if (have_posts()): while(have_posts()): the_post(); ?>
-  <?php if (!is_home() && !is_front_page()): ?>
-    <article>
-    <h2><?php the_title(); ?></h2>
-  <?php endif; ?>
-  <?php the_content(); ?>
-  <?php if (!is_home() && !is_front_page()): ?>
-    </article>
-  <?php endif; ?>
-<?php endwhile; endif; ?>
+<?php if (is_home()): ?>
+  <ol>
+  <?php if (have_posts()): while(have_posts()): the_post(); ?>
+    <li>
+      <a href="<?php the_permalink(); ?>">
+        <?php the_title(); ?>
+      </a>
+    </li>
+  <?php endwhile; endif; ?>
+  </ol>
+<?php else: ?>
+  <?php if (have_posts()): while(have_posts()): the_post(); ?>
+    <?php the_content(); ?>
+  <?php endwhile; endif; ?>
+<?php endif; ?>
 <?php get_footer(); ?>
