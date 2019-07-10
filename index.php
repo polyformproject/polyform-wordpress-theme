fd1a3180a77339f97a45100334b95b7e4b7852ba
Kyle E. Mitchell <kyle@kemitchell.com>
2019-07-09 18:28:33 -0700

    Show the dates of posts

--- index.php
+++ index.php
@@ -13,6 +13,7 @@
   <?php if (have_posts()): while(have_posts()): the_post(); ?>
     <?php if(!is_front_page()): ?>
       <h2><?php the_title(); ?></h2>
+      <p class=dateline><?php the_time(); ?></p>
     <?php endif; ?>
     <?php the_content(); ?>
   <?php endwhile; endif; ?>
