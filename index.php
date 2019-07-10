2e09cd1f727d97e57dc7c646a4d967119aa686d2
Kyle E. Mitchell <kyle@kemitchell.com>
2019-07-09 18:22:19 -0700

    Improve blog post display

--- index.php
+++ index.php
@@ -1,8 +1,12 @@
 <?php get_header(); ?>
 <?php if (have_posts()): while(have_posts()): the_post(); ?>
   <?php if (!is_home() && !is_front_page()): ?>
+    <article>
     <h2><?php the_title(); ?></h2>
   <?php endif; ?>
   <?php the_content(); ?>
+  <?php if (!is_home() && !is_front_page()): ?>
+    </article>
+  <?php endif; ?>
 <?php endwhile; endif; ?>
 <?php get_footer(); ?>
